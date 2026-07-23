1b53d63bcf172ee7889690423fbdd0d4209e9205
TonyBaloyi <221254110@mycput.ac.za>
2026-07-23 19:09:53 +0200

    *Was missing Factory so i added it

--- backend/src/main/java/za/ac/cput/campus_events/entity/Registration.java
+++ backend/src/main/java/za/ac/cput/campus_events/entity/Registration.java
@@ -1,3 +1,8 @@
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "registrations")
 public class Registration {
