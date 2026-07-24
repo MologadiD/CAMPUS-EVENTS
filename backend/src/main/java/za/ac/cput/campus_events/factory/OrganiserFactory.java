07fafe5fa1453c5cfa365454a42e34420dfb5af1
Leeroy Chakanyuka <168822625+leeroy-chakanyuka@users.noreply.github.com>
2026-07-24 15:45:26 +0200

    * Fixed compile time errors
    * Added Factory validation
    
    * Changed builders to use set Naming convention
    
    * Moved PromoCode to appropriate folder

--- backend/src/main/java/za/ac/cput/campus_events/factory/OrganiserFactory.java
+++ backend/src/main/java/za/ac/cput/campus_events/factory/OrganiserFactory.java
@@ -5,16 +5,38 @@ Student No: 231016263
  */
 
 import za.ac.cput.campus_events.domain.Organiser;
+import za.ac.cput.campus_events.util.EmailValidator;
+
 import java.time.LocalDateTime;
 
 public class OrganiserFactory {
+
+
     public static Organiser createOrganiser(String firstName, String lastName, String email, String role) {
+        //TODO : THESE CAN BE EXTRACTED INTO A METHOD SINCE ITS THE SAME CHECK FOR MOST THE STRINGS
+        if(firstName == null || firstName.length() < 3){
+            return null;
+        }
+
+        if(lastName == null || lastName.length() < 3){
+            return null;
+        }
+
+        if(email == null || email.length() < 3){
+            return null;
+        }
+
+        if(!EmailValidator.isValid(email)){
+            // TODO: MAKE THE REGEX DETECT UNI EMAILS, IE : AC.ZA
+            return null;
+        }
+
         return new Organiser.Builder()
-                .firstName(firstName)
-                .lastName(lastName)
-                .email(email)
-                .role(role)
-                .createdAt(LocalDateTime.now())
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setRole(role)
+                .setCreatedAt(LocalDateTime.now())
                 .build();
     }
 }
