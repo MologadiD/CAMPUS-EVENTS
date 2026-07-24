6ddba6467ddfcd0f60439a2bd2440913a4d23b25
Faith <222297204@mycput.ac.za>
2026-07-24 12:11:21 +0200

    [Event] Repository - Persistence Layer (#16)

--- backend/src/main/java/za/ac/cput/campus_events/repository/EventRepository.java
+++ backend/src/main/java/za/ac/cput/campus_events/repository/EventRepository.java
@@ -1,4 +1,10 @@
 package za.ac.cput.campus_events.repository;
 
-public interface EventRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.campus_events.domain.Event;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
 }
