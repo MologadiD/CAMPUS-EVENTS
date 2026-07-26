61fc629296c2028e3f66be8ec35e6fffeb99a566
TonyBaloyi <221254110@mycput.ac.za>
2026-07-26 22:07:13 +0200

    #21 PromoCode Repository

---
+++ backend/src/main/java/za/ac/cput/campus_events/repository/PromoCodeRepository.java
@@ -0,0 +1,25 @@
+package za.ac.cput.campus_events.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import za.ac.cput.campus_events.domain.PromoCode;
+
+
+import java.util.Optional;
+
+public interface PromoCodeRepository extends JpaRepository<PromoCode, String> {
+
+    Optional<PromoCode> findByCode(String code);
+
+    @Query("""
+            SELECT COUNT(t) > 0
+            FROM Ticket t
+            WHERE t.studentId = :studentId
+            AND t.promoCodeId = :promoCodeId
+            """)
+    boolean existsRedemptionByStudentIdAndPromoCodeId(
+            @Param("studentId") Long studentId,
+            @Param("promoCodeId") Long promoCodeId);
+
+}
