a5a406f720f6524b6637083429f52193b23b38e1
MologadiD <dikgalemologadi01@gmail.com>
2026-07-27 20:37:46 +0200

    Services

---
+++ backend/src/main/java/za/ac/cput/campus_events/service/IFacultyService.java
@@ -0,0 +1,16 @@
+package za.ac.cput.campus_events.service;
+/*
+Mologadi Dikgale
+Student No: 231016263
+ */
+import za.ac.cput.campus_events.domain.Faculty;
+import java.util.List;
+import java.util.Optional;
+
+public class IFacultyService {
+    Faculty save(Faculty faculty);
+    Optional<Faculty> findById(Long id);
+    List<Faculty> findAll();
+    void deleteById(Long id);
+    void deactivate(Long facultyId);
+}
---
+++ backend/src/main/java/za/ac/cput/campus_events/service/IOrganiserService.java
@@ -0,0 +1,21 @@
+package za.ac.cput.campus_events.service;
+/*
+Mologadi Dikgale
+Student No: 231016263
+ */
+
+import za.ac.cput.campus_events.domain.Organiser;
+import za.ac.cput.campus_events.domain.Event;
+import java.util.List;
+import java.util.Optional;
+
+public interface IOrganiserService {
+    Organiser save(Organiser organiser);
+    Optional<Organiser> findById(Long id);
+    List<Organiser> findAll();
+    void deleteById(Long id);
+    Organiser registerOrganiser(Organiser organiser, Long facultyId);
+    Event createEvent(Long organiserId, Event event);
+    Event updateEvent(Long organiserId, Event event);
+    void closeEvent(Long organiserId, Long eventId);
+}
