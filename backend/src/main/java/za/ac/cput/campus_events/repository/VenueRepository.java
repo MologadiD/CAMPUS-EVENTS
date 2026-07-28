0289e177bd35908b5482d83f49f42c195fab7421
Faith <222297204@mycput.ac.za>
2026-07-28 11:09:03 +0200

    [Venue] Repository: add plain CRUD interface (#46)

--- backend/src/main/java/za/ac/cput/campus_events/repository/VenueRepository.java
+++ backend/src/main/java/za/ac/cput/campus_events/repository/VenueRepository.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Repository;
 import za.ac.cput.campus_events.domain.Venue;
 
 @Repository
-public interface git add src/main/java/za/ac/cput/campus_events/repository/VenueRepository.java
-git commit -m "[Venue] Repository: add plain CRUD interface (#46)"
-VenueRepository extends JpaRepository<Venue, Long> {
+public interface VenueRepository extends JpaRepository<Venue, Long> {
     // Plain CRUD is automatically provided by JpaRepository
-    // No extra methods needed unless you want custom queries later
+    // No extra methods needed unless custom queries are added later
 }
